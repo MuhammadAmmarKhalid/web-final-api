e700f8ed42247097d413013cb4c2bf7599fc97f3
Muhammad Ammar Khalid <ammar.chawla12@gmail.com>
2021-01-06 00:50:13 +0500

    ok

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = "mongodb://ammar1:ammar1@cluster0-shard-00-00.ue8tr.mongodb.net:27017,cluster0-shard-00-01.ue8tr.mongodb.net:27017,cluster0-shard-00-02.ue8tr.mongodb.net:27017/finallab66?ssl=true&replicaSet=atlas-9qkwci-shard-0&authSource=admin&retryWrites=true&w=majority";
+const URI = "mongodb://ammar1:ammar1@cluster0-shard-00-00.ue8tr.mongodb.net:27017,cluster0-shard-00-01.ue8tr.mongodb.net:27017,cluster0-shard-00-02.ue8tr.mongodb.net:27017/finallab?ssl=true&replicaSet=atlas-9qkwci-shard-0&authSource=admin&retryWrites=true&w=majority";
 const connectDB = async() => {
     await mongoose.connect(URI, {
         useUnifiedTopology:true,
